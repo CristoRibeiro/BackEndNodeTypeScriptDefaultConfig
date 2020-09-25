5769ef5e61474a52a79b12dbe55bbbd741cb5ce4
CristoRibeiro <cristianosrsilva@gmail.com>
2020-09-24 22:33:23 -0300

    BackEndNodeDefaulConfig

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/barber', (request, response) => {
+  const { name } = request.query;
+
+  return response.json({ message: `Hello Barber ${name}!!!!` });
+});
+
+app.listen(3333, () => {
+  console.log('Server started with success !');
+});
